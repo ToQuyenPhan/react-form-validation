9466e7a54234e7498a464acecf0decea9ae77dbe
ToQuyenPhan <duonglh024@gmail.com>
2024-03-07 09:24:51 +0700

    done

--- src/Form.js
+++ src/Form.js
@@ -13,7 +13,6 @@ const Form = () => {
     return (
         <>
             <div className="form-container">
-                <span className="close-btn">X</span>
                 <div className="form-content-left">
                     <img src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                         alt="welcome-image" className="form-img" />
